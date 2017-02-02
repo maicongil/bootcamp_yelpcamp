f7227e8700e0fc183024e9359de6e8b50c6f5bc5
maicon <gugamaicon@gmail.com>
2017-02-02 20:06:57 -0200

    Add persistence with mongo and mongoose to campgrounds

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 
-var campgrounds =[
-    {name : "Camp 1", image : "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name : "Camp 2", image : "http://www.photosforclass.com/download/2602356334"},
-    {name : "Camp 3 ", image : "http://www.photosforclass.com/download/7121865553"}
-];
+var campgroundSchema = new mongoose.Schema({
+    name : String, 
+    image : String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
     res.render("landing")
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds : campgrounds});
+    Campground.find({}, function(error, campgrounds){
+        if(error){
+            console.log(error);
+        }else{
+            res.render("campgrounds", {campgrounds : campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name : name, image : image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    
+    Campground.create(
+        newCampground, function(error, campground){
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
